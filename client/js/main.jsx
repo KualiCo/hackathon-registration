81f5a69428b86c33d3d83d02960b52835f02378c
jkneal <jkneal@indiana.edu>
2014-09-30 12:05:43 -0400

    Inclusing stylus files in webpack

--- client/js/main.jsx
+++ client/js/main.jsx
@@ -1,5 +1,9 @@
 var React = require('react');
+
 var routes = require('./routes.jsx');
-console.log("Loaded Index fixed", routes);
+console.log("Loaded routes", routes);
+
+var styles = require('../css/app.styl');
+console.log("Loaded Styles", styles);
 
 React.renderComponent(routes, document.getElementById('main'));
